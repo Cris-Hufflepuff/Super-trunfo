892b233b934bf49e6c04c2f50bfa2347e866a733
Cris-Hufflepuff <81834397+Cris-Hufflepuff@users.noreply.github.com>
2022-03-16 09:57:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,328 @@
+var cartaDragaoFinalCibernetico = {
+    nome: "Dragão Final Cibernético",
+    imagem:" https://i.pinimg.com/474x/b8/ba/ba/b8baba8af97d9192900906732522763b.jpg",
+    atributos: {
+       ataque: 4080,
+       defesa: 2800,
+       classe: 10
+       
+    }
+}
+
+var cartaDragaoBrancoDeOlhosAzuis = {
+    nome: "Dragão Branco De Olhos Azuis",
+    imagem:"https://i.pinimg.com/736x/c4/81/21/c481212536b34b4afa43f9850ab518f8.jpg",
+    atributos: {
+       ataque:3000,
+       defesa:2500,
+       classe: 8
+    
+    }
+}
+
+var cartaHamonSenhorDoTrovao = {
+    nome: "Hamon, Senhor Do Trovão",
+    imagem:"https://i.pinimg.com/originals/67/3f/4c/673f4cd4029ff43e224c9df31aeeccf6.jpg",
+    atributos: {
+       ataque:4000,
+       defesa:4000,
+       classe: 10
+    
+    }
+}
+
+var cartaViceDragao  = {
+    nome: "Vice-Dragão ",
+    imagem:"https://http2.mlstatic.com/D_NQ_NP_511715-MLB25269127731_012017-O.jpg",
+    atributos: {
+       ataque:2000,
+       defesa:2400,   
+       classe:  4
+
+      
+     }
+}
+
+var cartaDragaoAladoDeRA = { // a carta que vence de todas
+    nome: "Dragão Alado De RA",
+    imagem:"https://i.pinimg.com/originals/0b/3a/35/0b3a3535fce27a486d8b6b9b22144fe5.jpg",
+    atributos: {
+       ataque: "?????",
+       defesa: "?????",
+       classe: "??"
+    
+    }
+}
+
+var cartaDragaoExplosivo = {
+    nome: "Dragão Explosivo",
+    imagem:"https://mypcards.com/img/3/498/yugioh_gld3_en012/yugioh_gld3_en012_en.jpg",
+    atributos: {
+       ataque:1000,
+       defesa:0,
+       classe: 3
+    
+    }    
+}
+
+var cartaDragaoDoBrilho = {
+    nome: "Dragão Do Brilho",
+    imagem:"https://i.pinimg.com/originals/96/3f/e1/963fe132b26eb427b313d508a38477f5.jpg",
+    atributos: {
+       ataque:1900,
+       defesa:1600,  
+       classe: 4
+    
+    }    
+}
+
+var cartaDragaoNegroDeOlhosVermelhos = {
+    nome: "Dragão Negro De Olhos Vermelhos",
+    imagem:"http://4.bp.blogspot.com/-DlVI0chsTvw/VOCsWstSd0I/AAAAAAAAAPA/NNQXXBN8chY/s1600/Yugioh-Card-Maker_910dd05a-5e4a-4d88-83f8-816dd6446a15%5B1%5D.jpg",
+    atributos: {
+       ataque:2400,
+       defesa:2000,  
+       classe: 7
+    
+    }
+}
+
+var cartaDragaoBrancoEPreto = {
+    nome: "Dragâo Branco E Preto",
+    imagem:"https://mypcards.com/img/3/580/yugioh_ladpp_001/yugioh_ladpp_001_en.jpg",
+    atributos: {
+       ataque:2800,
+       defesa:2400,  
+       classe: 8
+    
+    }
+}
+
+var cartaDragaoAmuleto = {
+    nome: "Dragão Amuleto",
+    imagem:"https://mypcards.com/img/3/399/yugioh_drlg_001/yugioh_drlg_001_en.jpg",
+    atributos: {
+       ataque:2900, 
+       defesa:2500,   
+       classe: 8
+    
+    }
+}
+
+var cartaBabyDragao = {
+    nome: "Baby Dragão",
+    imagem:"http://www.amigosdoforum.com.br/wp-content/uploads/2014/06/cartasnostalgia7.jpg",
+    atributos: {
+       ataque:1200,
+       defesa:700, 
+       classe: 3
+    
+    }
+}
+
+var cartaSenhorDosDragao = {
+    nome: "Senhor Dos Dragão",
+    imagem:"https://www.solosagrado.com.br//images/produtos/w200/h292/7422.jpg",
+    atributos: {
+       ataque:1200,
+       defesa:1100,
+       classe: 4 
+    
+    }
+}
+
+var cartaDragaoDaPoeriaEstelar = {
+    nome: "Dragão Da Poeria Estelar",
+    imagem:"https://mypcards.com/img/3/669/yugioh_shsp-ense1/yugioh_shsp-ense1_pt.jpg",
+    atributos: {
+       ataque:2500,
+       defesa:2000,   
+       classe: 8
+    
+    }
+}
+
+var cartaDragaoDeCincoCabeças = {
+    nome: "Dragão De Cinco Cabeças",
+    imagem:"https://images.uncyc.org/pt/thumb/c/ce/FGD.jpg/300px-FGD.jpg",
+    atributos: {
+       ataque:5000,
+       defesa:5000,  
+       classe: 12
+    
+    }
+}
+
+var cartaDragaoDeCristal = {
+    nome: "Dragão De Cristal",
+    imagem:"https://i.pinimg.com/474x/d6/1f/25/d61f25a2c61bb179318de4c8b9e25ed0.jpg",
+    atributos: {
+       ataque:2500,
+       defesa:1000,   
+       classe: 6
+    
+    }
+}
+
+var cartaDragaoProtoCyber = {
+    nome: "Dragão Proto-Cyber",
+    imagem:"https://images-na.ssl-images-amazon.com/images/I/51SJqrMzhYL._AC_.jpg",
+    atributos: {
+       ataque:1100,
+       defesa:600,   
+       classe: 3
+    
+    }
+}
+
+var cartaDragaoLaserCibernético = {
+    nome:"Dragão Laser Cibernético" ,
+    imagem:"https://i.pinimg.com/474x/9f/e8/6d/9fe86d4596d907dbbbefd30cc93d3625.jpg" ,
+    atributos: {
+       ataque:2400 ,
+       defesa:1800 ,
+       clase: 7
+    }
+}
+
+var cartaDragaoOlhosVermelhosDeMetalNegro = {
+    nome:"Dragão Olhos Vermelhos De Metal Negro",
+    imagem:" https://mypcards.com/img/3/605/yugioh_mipack_030/yugioh_mipack_030_en.jpg",
+    atributos: {
+       ataque:2800,
+       defesa:2400,
+       clase: 8
+    }
+}
+
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaDragaoOlhosVermelhosDeMetalNegro,cartaDragaoLaserCibernético,cartaDragaoProtoCyber,cartaDragaoDeCristal, cartaDragaoDeCincoCabeças, cartaDragaoDaPoeriaEstelar, cartaSenhorDosDragao, cartaBabyDragao,cartaDragaoAmuleto, cartaDragaoBrancoEPreto, cartaDragaoNegroDeOlhosVermelhos, cartaDragaoDoBrilho, cartaDragaoExplosivo, cartaDragaoAladoDeRA, cartaViceDragao, cartaHamonSenhorDoTrovao, cartaDragaoBrancoDeOlhosAzuis, cartaDragaoFinalCibernetico]
+
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+
+function atualizaQuantidadeDeCartas(){
+    var divQuantidadeCartas = document.getElementById('quantidade-cartas') 
+    var html = "Quantidade de cartas no jogo: " + cartas.length
+    divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar(){
+    var divPlacar = document.getElementById('placar')
+    var html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Maquina"
+    divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartas.splice(numeroCartaJogador, 1)
+    
+    cartaJogador = cartas[numeroCartaJogador]
+    
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+
+    exibeCartaJogador()
+}
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+    
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+       if (radioAtributo[i].checked) {
+         return radioAtributo[i].value
+       }
+   }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+    
+    if (cartaJogador.[atributoSelecionado] == cartaDragaoAladoDeRA[atributoSelecionado]){
+      htmlResultado = '<p class = "resultad-final"> Venceu!!!</p>'
+      pontosJogador++//soma 1 ponto
+   }else if (cartaMaquina[atributoSelecionado] == cartaDragaoAladoDeRA[atributoSelecionado]){
+      htmlResultado = '<p class = "resultad-final"> Perdeu!!!</p>'
+      pontosMaquina++
+   }else if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+      htmlResultado = '<p class="resultado-final">Venceu!!!</p>'
+      pontosJogador++//soma 1 ponto
+   }else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+      htmlResultado ='<p class="resultado-final">Perdeu!!!</p>'
+      pontosMaquina++
+   }else {
+       htmlResultado = '<p class="resultado-final">Empate!!!</p>'
+   }
+  
+    if (cartas.length == 0){
+     alert("Fim de Jogo!!!")
+       if (pontosJogador>pontosMaquina){
+         htmlResultado = '<p class="resultado-final">Venceu!!!</p>'
+       } else if (pontosMaquina>pontosJogador){
+         htmlResultado = '<p class="resultado-final">Perdeu!!!</p>'
+       }else{
+         '<p class="resultado-final">Empate!!!</p>'
+       }
+   }else{
+     document.getElementById('btnProximaRodada').disabled = false 
+   }
+
+    divResultado.innerHTML = htmlResultado
+    document.getElementById('btnJogar').disabled = true
+    
+    exibeCartaMaquina()
+    atualizaPlacar()
+    atualizaQuantidadeDeCartas()   
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+   
+function proximaRodada(){
+    var divCartas = document.getElementById('cartas')
+    divCartas.innerHTML = `<div id= "carta-jogador" class= "carta"></div><div id= "carta-maquina" class= "carta"></div>`// crase
+    document.getElementById('btnSortear').disabled = false
+    document.getElementById('btnJogar').disabled = true
+    document.getElementById('btnProximaRodada').disabled = true
+    var divResultado = document.getElementById('resultado')
+    divResultado.innerHTML = ""
+}
